43abd1a666f00e1392308b4f0e484520f69cecc8
fajr1mm <fajrimaulana402@gmail.com>
2024-01-17 13:37:02 +0700

    fix methode

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/predict_weather', methods=['GET'])
+def predict_weather():
+  Println('Hello  World')
 
 if __name__ == '__main__':
     app.run(debug=True)
