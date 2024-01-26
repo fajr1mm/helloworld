c949f2b976e72aa910103081c39dc635b9bea3c7
fajr1mm <fajrimaulana402@gmail.com>
2024-01-26 15:20:51 +0700

    fix app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/predict_weather', methods=['GET'])
 def predict_weather():
-  Println('Hello  World')
+  return Println('Hello  World')
 
 if __name__ == '__main__':
     app.run(debug=True)
